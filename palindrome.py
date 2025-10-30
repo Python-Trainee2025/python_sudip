b994c79db369d56e445c2c982dceed0eeb6c18b8
Sudip Bhandari <subhandari@infics.com>
2025-10-30 10:31:49 +0545

    delete unnecessary files

--- palindrome.py
+++
@@ -1,8 +0,0 @@
-
-value = input("Enter a word or phrase: ")
-cleaned_value = value.replace(" ", "").lower()
-x=cleaned_value[::-1]
-if cleaned_value == x:
-    print("the word is palindrome.")
-else:
-    print("the word is not a palindrome.")
--- unique words.py
+++
@@ -1,6 +0,0 @@
-import string
-text = input("Enter a sentence or paragraph: ")
-cleaned_text = text.translate(str.maketrans('', '', string.punctuation)).lower()
-words = cleaned_text.split()
-unique_words = set(words)
-print(f"Number of unique words: {len(unique_words)}")
